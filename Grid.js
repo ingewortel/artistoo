f11ab952496c06e153737a1f304c08e43aaa5e27
jtextor <johannes.textor@gmx.de>
2019-03-08 14:12:42 +0100

    new grid class

---
+++ Grid.js
@@ -0,0 +1,29 @@
+
+class Grid {
+	constructor( field_size, torus = true ){
+		this.extents = field_size
+		this.torus = torus
+		this.X_BITS = 1+Math.floor( Math.log2( this.extents[0] - 1 ) )
+		this.Y_BITS = 1+Math.floor( Math.log2( this.extents[1] - 1 ) )
+		this.midpoint = this.extents.map( i => Math.round((i-1)/2) )
+		this.Y_MASK = (1 << this.Y_BITS)-1
+		this.dy = 1 << this.Y_BITS // for neighborhoods based on pixel index
+	}
+
+	setpixi( i, t ){
+		this._pixels[i] = t
+	}
+	pixti( i ){
+		return this._pixels[i]
+	}
+
+	* pixels() {
+		for( let i = 0 ; i < this._pixels.length ; i ++ ){
+			if( this._pixels[i] != 0 ){
+				yield [this.i2p(i),this._pixels[i]]
+			}
+		}
+	}
+}
+
+export default Grid
