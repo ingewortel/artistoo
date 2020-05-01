a6751153e1b30d17f508eb49c9699ff8210e599d
Inge Wortel <ingewortel@gmail.com>
2020-05-01 18:37:17 +0200

    fixed docs problem

---
+++ docs/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "jasmine" : true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
