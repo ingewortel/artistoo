d8138e96d9c2836fa7f7318e44e5ff28338e109c
Inge Wortel <ingewortel@gmail.com>
2021-03-04 22:42:42 +0100

    slight restructuring of the test in GridManipulatorSpec so that it is not dependent on the Simulation class anymore. Added a test for the killCell method.

--- spec/grid/GridManipulatorSpec.js
+++ spec/grid/GridManipulatorSpec.js
@@ -4,45 +4,64 @@
 describe("GridManipulator", function () {
 	let CPM = require("../../build/artistoo-cjs.js")
 	//eslint-disable-next-line no-unused-vars
-	let sim
+	let C, G
 
 	let setupSim = function ( ) {
 
-		let config = {
-			field_size : [50,50],
-			conf : {
-				// Basic CPM parameters
-				torus : [true,true],
-				T : 10,
-				J: [[0,10], [10,0]],
-				LAMBDA_V: [0,5],
-				V: [0,500],
-				LAMBDA_P: [0,2],
-				P : [0,260]
-			},
+		C = new CPM.CPM( [50,50], {
+			T : 10,
+			J: [[0,10], [10,0]],
+			V : [0,10],
+			LAMBDA_V : [0,5]
+		} )
 
-			// Simulation setup and configuration
-			simsettings : {
-				NRCELLS : [1],
-				BURNIN : 10,
-				RUNTIME : 10,
-				CANVASCOLOR : "eaecef",
-				STATSOUT : { browser: false, node: false },
-			}
-		}
-
-
-		sim = new CPM.Simulation( config, {} )
-		sim.initializeGrid()
-		sim.run()
+		G = new CPM.GridManipulator( C )
+		G.seedCell(1 )
+		for( let t = 0; t < 10; t++ ){ C.monteCarloStep() }
 	}
 
 	describe(" [ unit tests ] ", function () {
 
 		/** @test {GridManipulator#seedCellAt} */
-		it("should throw error when trying to seed outside of grid", function () {
+		it("seedCellAt should not throw error normally", function () {
 			setupSim()
-			expect( function() { sim.gm.seedCellAt( 1, [300,-5]) }).toThrow()
+			expect( function() { G.seedCellAt( 1, [0,0] ) }).not.toThrow()
+		})
+		it("seedCellAt should throw error when trying to seed outside of grid", function () {
+			setupSim()
+			expect( function() { G.seedCellAt( 1, [300,-5]) }).toThrow()
+		})
+
+		/** @test {GridManipulator#killCell} */
+		it("should remove a cellID from the grid", function () {
+			setupSim()
+			const cid = C.cellIDs().next().value
+			expect( function() { G.killCell(cid) }).not.toThrow()
+			let pixels = []
+			for( let [p,i] of C.grid.pixels() ){
+
+				if( i.toString() === cid ){
+					pixels.push(p)
+				}
+			}
+			expect( pixels.length ).toEqual( 0 )
+		})
+
+		it("should not mess up cellKinds etc", function () {
+			setupSim()
+			const cid = C.cellIDs().next().value
+
+			// seed a second cell
+			G.seedCellAt( 1, [1,1] )
+			// kill the first cell
+			G.killCell(cid)
+
+			// check that all pixt() and cellKind() are still defined.
+			for( let [p,i] of C.grid.pixels() ){
+				expect( C.pixt(p) ).toBeDefined()
+				expect( C.cellKind(i) ).toBeDefined()
+				expect( C.cellKind(i) ).toEqual( 1 )
+			}
 		})
 
 	})
