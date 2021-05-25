d032e4b9aa5b37ed57e13eff5197e081d7fe423d
Inge Wortel <ingewortel@gmail.com>
2021-05-25 19:10:52 +0200

    removed old stuff

--- misc/add-mathjax.bash
+++
@@ -1,9 +0,0 @@
-echo "<script>"
-echo "MathJax = {"
-echo -e "\ttex: {"
-echo -e "\t\tinlineMath: [['$', '$']]"
-echo -e "\t"}
-echo "};"
-echo "</script>"
-echo "<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>"
-echo "<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>"
